aec6715fc122f57889152aa257072918f1e7abb8
Roshan Geo Roy <tve19ec04@cet.ac.in>
2021-02-16 23:46:49 +0530

    All initial steps done. Publishing soon

--- scripts.js
+++ scripts.js
@@ -7,7 +7,9 @@
 //     {
 //       compliment.innerHTML='${data.compliment}'
 //     })
+
 // }
+
 function fetchData()
 {
   fetch("https://complimentr.com/api")
@@ -23,3 +25,29 @@ function fetchData()
   )
 }
 fetchData();
+
+// function updateClock()
+// {
+//   var now= new Date();
+//   var
+//       mo=now.getMonth(),
+//       dnum=now.getDate(),
+//       yr=now.getFullYear(),
+//       h=now.getHours(),
+//       min=now.getMinutes(),
+//       sec=now.getSeconds();
+// var ids=["month","date","year","hours","mins","secs"] ;
+//  var   values=["mo","dnum","yr","h","min","sec"];
+//  var months=["January","February","March","April","May","June","July","August","September","October","November","December"];
+// // for(var i =0;i<6;i++)
+// // {
+// //   document.getElementById(ids[i]).innerHTML=values[i];
+// // }
+// document.getElementById("secs").firstChild.nodeValue=sec;
+// console.log(mo);
+// }
+// function initClock()
+// {
+//   updateClock();
+//   window.setInterval("updateClock()",1);
+// }
